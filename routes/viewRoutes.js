a81946a155a835701b75e81e37f56ee87fd9fd23
Sarvasva <sarvasva_g@cs.iitr.ac.in>
2025-01-08 12:31:33 +0530

    Fix bug

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  viewController.setLoggedIn,
-  viewController.getOverview,
-);
+router.get('/', viewController.setLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', viewController.setLoggedIn, viewController.getTour);
 router.get('/login', viewController.setLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
